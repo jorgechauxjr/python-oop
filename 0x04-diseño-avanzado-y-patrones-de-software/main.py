0ba15127dd711a0300b7d50ddb96a7abac40ffb5
jorge <chauxjorge@hotmail.com>
2026-06-01 14:33:11 -0500

    [Add] 17 Lectura de archivos JSON para reconstruir objetos en Python

---
+++ 0x04-diseño-avanzado-y-patrones-de-software/main.py
@@ -0,0 +1,43 @@
+import sys
+from exceptions import LibroNoDisponibleError, UsuarioNoEncontradoError
+from persistencia import Persistencia
+
+persistencia = Persistencia()
+biblioteca = persistencia.cargar_datos()
+
+print("Bienvenido a Platzi Biblioteca")
+
+print("Libros disponibles:")
+for libro in biblioteca.libros_disponibles():
+    print(libro.descripcion_completa)
+print()
+
+cedula = input("Digite el numero cedula: ")
+try:
+    usuario = biblioteca.buscar_usuario(cedula)
+    # print(usuario.cedula, usuario.nombre)
+    print(usuario.nombreCompleto)
+except UsuarioNoEncontradoError as e:
+    print(e)
+    # Finalizar el programa
+    sys.exit()
+
+titulo = input("Digite el titulo del libro: ")
+try:
+    libro = biblioteca.buscar_libro(titulo)
+    print(f"El libro que selecionaste es: {libro}")
+except LibroNoDisponibleError as e:
+    print(e)
+    # Finalizar el programa
+    sys.exit()
+
+resultado = usuario.solicitar_libro(libro.titulo)
+print(f"\n{resultado}")
+
+try:
+    resultado_prestar = libro.prestar()
+    print(f"\n{resultado_prestar}")
+except LibroNoDisponibleError as e:
+    print(e)
+
+persistencia.guardar_datos(biblioteca)
